d2a648f12dcbbaa48128336c7012a1ac93241ca7
Nouhaila1937 <lahsaouinouhaila@gmail.com>
2025-03-15 11:04:37 +0000

    la création d'entite jpa;

--- src/main/java/org/example/studentsapp/entities/Product.java
+++ src/main/java/org/example/studentsapp/entities/Product.java
@@ -4,11 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data //c'est du lombook il va ajouter des setters et des getters auto sans les faires
-
+@NoArgsConstructor @AllArgsConstructor
 public class Product {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) //c'est autoincrement
    private Long id;
