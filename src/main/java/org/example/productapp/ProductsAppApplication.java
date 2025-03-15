abca351f98cc5665ebd0edbc8569a0933b64bde3
Nouhaila1937 <lahsaouinouhaila@gmail.com>
2025-03-15 12:07:16 +0000

    repo /

--- src/main/java/org/example/productapp/ProductsAppApplication.java
+++ src/main/java/org/example/productapp/ProductsAppApplication.java
@@ -17,5 +17,8 @@ public class ProductsAppApplication implements CommandLineRunner {
 	}
 	@Override
 	public void run(String... args) throws Exception {
+	productRepository.save(new Product(null,"Computer",43000.0,2));
+	productRepository.save(new Product(null,"Printer",4300.0,20));
+	productRepository.save(new Product(null,"Phone",7300.0,10));
 	}
 }
